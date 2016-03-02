2dbac1c4fbd1f403c8c19606d106121b8e269de1
WU Yuxiang <ywubw@qycpu6.cse.ust.hk>
2016-03-03 00:55:01 +0800

    60 minutes tutorial finished

--- 60min_torch_tutorial/cifar.lua
+++ 60min_torch_tutorial/cifar.lua
@@ -58,8 +58,41 @@ net:add(nn.Linear(84, 10))                   -- 10 is the number of outputs of t
 net:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classification problems
 
 criterion = nn.ClassNLLCriterion()
+
+-- CUDA
+require 'cunn'
+net = net:cuda()
+criterion = criterion:cuda()
+trainset.data = trainset.data:cuda()
+
 trainer = nn.StochasticGradient(net, criterion)
 trainer.learningRate = 0.01
-trainer.maxIteration = 100
+trainer.maxIteration = 10
 trainer:train(trainset)
+
+print(classes[testset.label[100]])
+testset.data = testset.data:double()
+for i=1,3 do -- over each image channel
+    testset.data[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction    
+    testset.data[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+testset.data = testset.data:cuda()
+predicted = net:forward(testset.data[100])
+print(predicted:exp())
+for i=1,predicted:size(1) do
+    print(classes[i], predicted[i])
+end
+
+correct = 0
+for i=1,10000 do
+    local groundtruth = testset.label[i]
+    local prediction = net:forward(testset.data[i])
+    local confidences, indices = torch.sort(prediction, true)  -- true means sort in descending order
+    if groundtruth == indices[1] then
+        correct = correct + 1
+    end
+end
+
+print(correct, 100*correct/10000 .. ' % ')
 
