115787cb93fe86f7f4e510ebafce6cfd3d12d43c
Ubuntu <ubuntu@ip-172-31-1-221.us-west-1.compute.internal>
2016-03-02 16:28:26 +0000

    cifar.lua modified

--- 60min_torch_tutorial/cifar.lua
+++ 60min_torch_tutorial/cifar.lua
@@ -1,6 +1,6 @@
 -- os.execute('wget -c https://s3.amazonaws.com/torch7/data/cifar10torchsmall.zip')
 -- os.execute('unzip cifar10torchsmall.zip')
-require 'image'
+-- require 'image'
 trainset = torch.load('cifar10-train.t7')
 testset = torch.load('cifar10-test.t7')
 classes = {'airplane', 'automobile', 'bird', 'cat',
@@ -8,5 +8,58 @@ classes = {'airplane', 'automobile', 'bird', 'cat',
 print(trainset)
 print(#trainset.data)
 
-image.display(trainset.data[100])
+-- image.display(trainset.data[100])
 print(classes[trainset.label[100]])
+
+setmetatable(trainset, 
+    {__index = function(t, i) 
+                   return {t.data[i], t.label[i]} 
+               end});
+trainset.data = trainset.data:double() --convert data from ByteTensor to DoubleTensor
+
+function trainset:size()
+    return self.data:size(1)
+end
+
+print(trainset:size())
+-- image.display(trainset[33][1])
+print(trainset[33][2])
+
+redChannel = trainset.data[{ {}, {1}, {}, {}  }] 
+print(#redChannel)
+slicing = trainset.data[{ {150, 300}, {}, {}, {} }]
+
+mean = {}
+stdv = {}
+for i = 1, 3 do
+    mean[i] = trainset.data[{ {}, {i}, {}, {}  }]:mean() -- mean estimation
+    print('Channel ' .. i .. ', Mean: ' .. mean[i])
+    trainset.data[{ {}, {i}, {}, {}  }]:add(-mean[i]) -- mean subtraction
+    stdv[i] = trainset.data[{ {}, {i}, {}, {}  }]:std() -- std estimation
+    print('Channel ' .. i .. ', Standard Deviation: ' .. stdv[i])
+    trainset.data[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
+end
+
+require 'nn'
+
+net = nn.Sequential()
+net:add(nn.SpatialConvolution(3, 6, 5, 5)) -- 1 input image channel, 6 output channels, 5x5 convolution kernel
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.SpatialMaxPooling(2,2,2,2))     -- A max-pooling operation that looks at 2x2 windows and finds the max.
+net:add(nn.SpatialConvolution(6, 16, 5, 5))
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.SpatialMaxPooling(2,2,2,2))
+net:add(nn.View(16*5*5))                    -- reshapes from a 3D tensor of 16x5x5 into 1D tensor of 16*5*5
+net:add(nn.Linear(16*5*5, 120))             -- fully connected layer (matrix multiplication between input and weights)
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.Linear(120, 84))
+net:add(nn.ReLU())                       -- non-linearity 
+net:add(nn.Linear(84, 10))                   -- 10 is the number of outputs of the network (in this case, 10 digits)
+net:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classification problems
+
+criterion = nn.ClassNLLCriterion()
+trainer = nn.StochasticGradient(net, criterion)
+trainer.learningRate = 0.01
+trainer.maxIteration = 100
+trainer:train(trainset)
+
